f95f1ee614fe2794c819d213a06313488b5d85f0
David G Rosenberg <targendaz2@me.com>
2023-01-23 02:22:32 +0800

    Renamed app to data_in

--- data_in/apps.py
+++ data_in/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DatainConfig(AppConfig):
+class DataInConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'datain'
+    name = 'data_in'
