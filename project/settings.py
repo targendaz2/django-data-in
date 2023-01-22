c998ec0523abb943d4274b93b3d89b56ecbfd2c1
David G Rosenberg <targendaz2@me.com>
2023-01-23 03:28:01 +0800

    Created tests app

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'data_in'
+    'data_in',
+    'tests',
 ]
 
 MIDDLEWARE = [
